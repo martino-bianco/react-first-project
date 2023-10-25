b7c2ad6ce06f6b2e5140f448ab2244d0ab3bb115
martino-bianco <121939439+martino-bianco@users.noreply.github.com>
2023-10-25 19:48:39 +0200

    adding the CoreConcept

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,16 @@ function Header() {
   );
 }
 
+function CoreConcept() {
+  return (
+    <li>
+      <img src="" alt="" />
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>

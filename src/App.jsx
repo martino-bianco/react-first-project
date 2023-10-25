bcc59da4c02e8a5e65653bb39181523561257bad
martino-bianco <121939439+martino-bianco@users.noreply.github.com>
2023-10-25 20:03:28 +0200

    editing the first CoreConcept

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-
+import componentsImg from "./assets/components.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -20,12 +20,12 @@ function Header() {
   );
 }
 
-function CoreConcept() {
+function CoreConcept(props) {
   return (
     <li>
-      <img src="" alt="" />
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
+      <img src={props.image} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
   );
 }
@@ -35,6 +35,19 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
